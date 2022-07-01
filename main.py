ec7face9f96e263ef586a95f1b007ccdd9019151
fearaizor222 <97432287+fearaizor222@users.noreply.github.com>
2022-07-01 16:31:44 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import hikari
+import mangadex
+import lightbulb
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def findCoverArt(name):
+    api = mangadex.Api()
+    manga = api.get_manga_list(title = name)
+    manga = manga[0]
+    manga = api.get_cover(manga.coverId)
+    return manga.fetch_cover_image()
+
+Bot = lightbulb.BotApp(
+    token=os.getenv("DISCORD_TOKEN"),
+    default_enabled_guilds=(int(os.getenv("DISCORD_SERVER")))
+)
+
+@Bot.listen(hikari.GuildMessageCreateEvent)
+async def listenForManga(event):
+    if event.content.startswith("!manga-"):
+        try:
+            await event.message.respond(findCoverArt(event.content[7:]))
+        except:
+            await event.message.respond("No cover art")
+
+@Bot.command
+@lightbulb.command("hello", "hello user")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def hello(ctx):
+    await ctx.respond("Hello Human")
+
+Bot.run()
